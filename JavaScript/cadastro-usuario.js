1a3427249f7aa7b34f50ad4466defae6b6391971
MaestroN1k022 <nicolas.cavalini@aluno.faculdadeimpacta.com.br>
2021-12-14 21:22:47 -0300

    Update cadastro-usuario.js

--- JavaScript/cadastro-usuario.js
+++ JavaScript/cadastro-usuario.js
@@ -1,4 +1,4 @@
-class Validator {  
+class validator {  
     constructor() {
         this.validations = [
             'data-min-length',
@@ -7,6 +7,13 @@ class Validator {
 
     // iniciar a validação de todos os campos
     validate(form) {
+
+        // criação do método que irá limpar as validações
+        let currentValidations = document.querySelectorAll('form .error-validation');
+
+        if(currentValidations.length > 0) {
+            this.cleanValidations(currentValidations);
+        }
 
         //pegar os inputs
         let inputs = form.getElementsByTagName('input');
@@ -36,27 +43,45 @@ class Validator {
     minlength(input, minValue) {
         let inputLength = input.value.length;
         let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
+
         if(inputLength < minValue) {
             this.printMessage(input, errorMessage);
         }
+        else {
+            this.removeMessage(input);
+        }
     }
 
-    //metodo para imprimir mensagens de erro
-    printMessage(input, msg) {
-        let template = document.querySelector('.error-validation').cloneNode(true);
-        template.textContent = msg;
-        let inputParent = input.parentNode;
-        template.classList.remove('template');
-        inputParent.appendChild(template);
+        //metodo para imprimir mensagens de erro
+        printMessage(input, msg) {
+
+            let errorValidation= document.querySelector('.error-validation');
+    
+            if(errorValidation !== null && errorValidation !== undefined){
+            let template = errorValidation.cloneNode(true);          
+    
+            template.textContent = msg;
+    
+            let inputParent = input.parentNode;
+    
+            template.classList.remove('template');
+    
+            inputParent.appendChild(template);
+            }     
+        }
+
+    // limpa as validações da tela
+    cleanValidations(validations) {
+        validations.forEach(el => el.remove());
     }
 }
 
 let form = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
-let Validator = new Validator();
+let Validator = new validator();
 
 // evento que dispara as validações
 submit.addEventListener('click', function(e){
     e.preventDefault();
-    validator.validate(form);
+    Validator.validate(form);
 });
